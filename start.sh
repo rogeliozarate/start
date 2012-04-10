c9b0efc9e1f92d57bc9c76285292f3fda8ec7e12
Rogelio Zarate <i.am@rogeliozarate.com>
2012-04-09 22:20:05 -0500

    checking for directory name ok

--- start.sh
+++ start.sh
@@ -3,7 +3,13 @@ if [ $1 ]
 then
   echo "String \"$1\" is not null."
   BASE_DIR=/Users/cobi/Documents/code/
-  echo $BASE_DIR$1
+  DIR=$BASE_DIR$1
+  if [ -d "$DIR" ]; then
+    # Control will enter here if $DIRECTORY exists
+    echo "Directory ${DIR} exist"
+  else
+    echo "Directory ${DIR} does not exist"	
+  fi
 else  
   echo "app name is null, leaving the script"
   exit
